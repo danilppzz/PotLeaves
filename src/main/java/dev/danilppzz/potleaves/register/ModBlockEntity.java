8ecad6d6c9fc3cbc664c45c61f002b187a12779d
d4niz <josedanielmorebolopez@gmail.com>
2024-03-03 14:33:02 +0100

    update

--- src/main/java/dev/danilppzz/potleaves/register/ModBlockEntity.java
+++ src/main/java/dev/danilppzz/potleaves/register/ModBlockEntity.java
@@ -2,14 +2,9 @@ package dev.danilppzz.potleaves.register;
 
 import dev.danilppzz.potleaves.PotLeaves;
 import dev.danilppzz.potleaves.custom.CustomWoodenBox;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlockEntity {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, PotLeaves.MODID);
