f69d61e3e6ae6aed3dc05400c858a4e7120daf0f
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-04-21 17:35:06 +0200

    Fix PotLeaves tab

--- common/src/main/java/dev/danilppzz/potleaves/PotLeaves.java
+++ common/src/main/java/dev/danilppzz/potleaves/PotLeaves.java
@@ -1,18 +1,15 @@
 package dev.danilppzz.potleaves;
 
-import com.mojang.logging.LogUtils;
 import dev.danilppzz.potleaves.register.*;
-import org.slf4j.Logger;
 
 public class PotLeaves {
     public static final String MOD_ID = "potleaves";
-    private static final Logger LOGGER = LogUtils.getLogger();
 
     public static void init() {
-        ModTabs.TABS.register();
         ModBlocks.BLOCKS.register();
         ModItems.ITEMS.register();
         ModBlockEntity.BLOCK_ENTITIES.register();
         ModMenus.MENUS.register();
+        ModTabs.TABS.register();
     }
 }
--- common/src/main/java/dev/danilppzz/potleaves/register/ModTabs.java
+++ common/src/main/java/dev/danilppzz/potleaves/register/ModTabs.java
@@ -6,10 +6,7 @@ import dev.danilppzz.potleaves.PotLeaves;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-
-import java.util.Iterator;
 
 public class ModTabs {
     public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(PotLeaves.MOD_ID, Registries.CREATIVE_MODE_TAB);
@@ -17,5 +14,6 @@ public class ModTabs {
     public static final RegistrySupplier<CreativeModeTab> POT_LEAVES = TABS.register(PotLeaves.MOD_ID, () -> CreativeModeTab.builder(CreativeModeTab.Row.TOP, 0)
             .icon(() -> new ItemStack(ModBlocks.SMALL_LEAVE_POT.get()))
             .title(Component.translatable("itemGroup.potleaves"))
+            .displayItems(((itemDisplayParameters, output) -> ModItems.ITEMS.forEach(itemRegistrySupplier -> output.accept(itemRegistrySupplier.get()))))
             .build());
 }
