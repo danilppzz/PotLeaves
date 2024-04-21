c7b450feb9757aa87710bee215b1f7125643db4e
TonimatasDEV <amatascubero@gmail.com>
2024-04-21 17:01:00 +0200

    Fix Wooden Box hitbox

--- common/src/main/java/dev/danilppzz/potleaves/blocks/WoodenBox.java
+++ common/src/main/java/dev/danilppzz/potleaves/blocks/WoodenBox.java
@@ -1,39 +1,31 @@
 package dev.danilppzz.potleaves.blocks;
 
 import dev.danilppzz.potleaves.register.ModBlockEntity;
-import dev.danilppzz.potleaves.register.ModBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.DirectionProperty;
-import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class WoodenBox extends AbstractEntityBlock {
-    public static DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
-
     public WoodenBox(Properties properties) {
         super(properties.dynamicShape().noOcclusion());
     }
 
-    //@Override
-    //public @NotNull VoxelShape getShape(BlockState p_60555_, BlockGetter p_60556_, BlockPos p_60557_, CollisionContext p_60558_) {
-    //    Direction $$1 = p_60555_.getValue(FACING);
-    //    if ($$1 != Direction.NORTH && $$1 != Direction.SOUTH) {
-    //        return Block.box(4,0,2,12,7,14);
-    //    }
-//
-    //    return Block.box(2,0,4,14,7,12);
-    //}
+    @Override
+    public @NotNull VoxelShape getShape(BlockState p_60555_, BlockGetter p_60556_, BlockPos p_60557_, CollisionContext p_60558_) {
+        Direction direction = p_60555_.getValue(FACING);
+        if (direction != Direction.NORTH && direction != Direction.SOUTH) {
+            return Block.box(4,0,2,12,7,14);
+        }
+
+        return Block.box(2,0,4,14,7,12);
+    }
 
     @Nullable
     @Override
